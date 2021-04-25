30fd66078b008e30b9da5251b8de84b5c59c2038
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-04-25 08:13:37 -0300

    feat: go flags cli

--- 01-go_flags/main.go
+++ 01-go_flags/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	"log"
+	os "os"
 )
 
+var beers = map[string]string{
+	"01D9X58E7NPXX5MVCR9QN794CH": "Mad Jack Mixer",
+	"01D9X5BQ5X48XMMVZ2F2G3R5MS": "Keystone Ice",
+	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
+}
+
 func main() {
-	param := os.Args[1]
-	fmt.Println(param)
+	beersCmd := flag.NewFlagSet("beers", flag.ExitOnError)
+	flag.Parse()
+	if flag.NArg() == 0 {
+		log.Fatal("You must specified a command beers")
+	}
+	switch flag.Arg(0) {
+	case "beers":
+		ID := beersCmd.String("id", "", "find by ID")
+		err := beersCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if *ID != "" {
+			fmt.Println(beers[*ID])
+		} else {
+			fmt.Println(beers)
+		}
+	}
 }
