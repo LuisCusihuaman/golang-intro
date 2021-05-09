fe7053ccbd62d49fa5ff3f2794a099852ec697fa
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-05-09 11:05:49 -0300

    feat: http service with repository pattern

--- cmd/beers-cli/main.go
+++ cmd/beers-cli/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
+	beerscli "github.com/LuisCusihuaman/golang-introduction/internal"
 	"github.com/LuisCusihuaman/golang-introduction/internal/cli"
 	"github.com/LuisCusihuaman/golang-introduction/internal/storage/csv"
+	"github.com/LuisCusihuaman/golang-introduction/internal/storage/ontario"
 	"github.com/spf13/cobra"
 )
 
 //go run cmd/beers-cli/main.go beers --id 127
 func main() {
 
-	csvRepo := csv.NewRepository()
+	csvData := flag.Bool("csv", false, "load data from csv")
+	flag.Parse()
+
+	var repo beerscli.BeerRepo
+	repo = csv.NewRepository()
+
+	if !*csvData {
+		repo = ontario.NewOntarioRepository()
+	}
 
 	rootCmd := &cobra.Command{Use: "beers-cli"}
-	rootCmd.AddCommand(cli.InitBeersCmd(csvRepo))
+	rootCmd.AddCommand(cli.InitBeersCmd(repo))
 	_ = rootCmd.Execute()
 }
---
+++ internal/storage/ontario/repository.go
@@ -0,0 +1,41 @@
+package ontario
+
+import (
+	"encoding/json"
+	"fmt"
+	beerscli "github.com/LuisCusihuaman/golang-introduction/internal"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	productsEndpoint = "/products"
+	ontarioURL       = "http://ontariobeerapi.ca"
+)
+
+type beerRepo struct {
+	url string
+}
+
+// NewOntarioRepository fetch beers data from csv
+func NewOntarioRepository() beerscli.BeerRepo {
+	return &beerRepo{url: ontarioURL}
+}
+
+func (b *beerRepo) GetBeers() (beers []beerscli.Beer, err error) {
+	response, err := http.Get(fmt.Sprintf("%v%v", b.url, productsEndpoint))
+	if err != nil {
+		return nil, err
+	}
+
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(contents, &beers)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
